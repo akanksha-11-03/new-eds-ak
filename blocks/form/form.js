8fd2e201963b144edc30a240514c037f0a8335e7
akanksha-11-03 <Akanksha.Kasar@teknopoint.local>
2025-04-11 15:13:06 +0530

    new form form new block

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,14 +1,16 @@
-import createField from './form-fields.js';
+import createField from "./form-fields.js";
 
 async function createForm(formHref, submitHref) {
   const { pathname } = new URL(formHref);
   const resp = await fetch(pathname);
   const json = await resp.json();
 
-  const form = document.createElement('form');
+  const form = document.createElement("form");
   form.dataset.action = submitHref;
 
-  const fields = await Promise.all(json.data.map((fd) => createField(fd, form)));
+  const fields = await Promise.all(
+    json.data.map((fd) => createField(fd, form))
+  );
   fields.forEach((field) => {
     if (field) {
       form.append(field);
@@ -16,11 +18,13 @@ async function createForm(formHref, submitHref) {
   });
 
   // group fields into fieldsets
-  const fieldsets = form.querySelectorAll('fieldset');
+  const fieldsets = form.querySelectorAll("fieldset");
   fieldsets.forEach((fieldset) => {
-    form.querySelectorAll(`[data-fieldset="${fieldset.name}"`).forEach((field) => {
-      fieldset.append(field);
-    });
+    form
+      .querySelectorAll(`[data-fieldset="${fieldset.name}"`)
+      .forEach((field) => {
+        fieldset.append(field);
+      });
   });
 
   return form;
@@ -30,11 +34,14 @@ function generatePayload(form) {
   const payload = {};
 
   [...form.elements].forEach((field) => {
-    if (field.name && field.type !== 'submit' && !field.disabled) {
-      if (field.type === 'radio') {
+    if (field.name && field.type !== "submit" && !field.disabled) {
+      if (field.type === "radio") {
         if (field.checked) payload[field.name] = field.value;
-      } else if (field.type === 'checkbox') {
-        if (field.checked) payload[field.name] = payload[field.name] ? `${payload[field.name]},${field.value}` : field.value;
+      } else if (field.type === "checkbox") {
+        if (field.checked)
+          payload[field.name] = payload[field.name]
+            ? `${payload[field.name]},${field.value}`
+            : field.value;
       } else {
         payload[field.name] = field.value;
       }
@@ -44,20 +51,20 @@ function generatePayload(form) {
 }
 
 async function handleSubmit(form) {
-  if (form.getAttribute('data-submitting') === 'true') return;
+  if (form.getAttribute("data-submitting") === "true") return;
 
   const submit = form.querySelector('button[type="submit"]');
   try {
-    form.setAttribute('data-submitting', 'true');
+    form.setAttribute("data-submitting", "true");
     submit.disabled = true;
 
     // create payload
     const payload = generatePayload(form);
     const response = await fetch(form.dataset.action, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({ data: payload }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
     if (response.ok) {
@@ -72,31 +79,69 @@ async function handleSubmit(form) {
     // eslint-disable-next-line no-console
     console.error(e);
   } finally {
-    form.setAttribute('data-submitting', 'false');
+    form.setAttribute("data-submitting", "false");
     submit.disabled = false;
   }
 }
 
 export default async function decorate(block) {
-  const links = [...block.querySelectorAll('a')].map((a) => a.href);
-  const formLink = links.find((link) => link.startsWith(window.location.origin) && link.endsWith('.json'));
+  const links = [...block.querySelectorAll("a")].map((a) => a.href);
+  const formLink = links.find(
+    (link) => link.startsWith(window.location.origin) && link.endsWith(".json")
+  );
   const submitLink = links.find((link) => link !== formLink);
-  if (!formLink || !submitLink) return;
+  // if (!formLink || !submitLink) return;
 
-  const form = await createForm(formLink, submitLink);
+  const form = await createForm(formLink, "");
   block.replaceChildren(form);
+  //  var amountdis = parseFloat(document.getElementById("amount"));
+  //  amountdis.addEventListener("change", calculateSIP)
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const valid = form.checkValidity();
-    if (valid) {
-      handleSubmit(form);
-    } else {
-      const firstInvalidEl = form.querySelector(':invalid:not(fieldset)');
-      if (firstInvalidEl) {
-        firstInvalidEl.focus();
-        firstInvalidEl.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
+  amount.addEventListener("change", function () {
+    calculateSIP();
   });
+  years.addEventListener("change", function () {
+    calculateSIP();
+  });
+  returnRate.addEventListener("change", function () {
+    calculateSIP();
+  });
+  // form.addEventListener('submit', (e) => {
+  //   e.preventDefault();
+  //   const valid = form.checkValidity();
+  //   if (valid) {
+  //     handleSubmit(form);
+  //   } else {
+  //     const firstInvalidEl = form.querySelector(':invalid:not(fieldset)');
+  //     if (firstInvalidEl) {
+  //       firstInvalidEl.focus();
+  //       firstInvalidEl.scrollIntoView({ behavior: 'smooth' });
+  //     }
+  //   }
+  // });
 }
+
+function calculateSIP() {
+  var amount = parseFloat(document.getElementById("amount").value);
+  var years = parseInt(document.getElementById("years").value);
+  var returnRate = parseFloat(
+    document.getElementById("returnRate").value
+  );
+  
+  // if (isNaN(amount) || isNaN(years) || isNaN(returnRate)) {
+  //   document.getElementById("result").innerHTML = "Please enter valid values.";
+  //   return;
+  // }
+
+  var months = years * 12;
+  var monthlyRate = returnRate / 12 / 100;
+
+  var futureValue =
+    amount *
+    (((Math.pow(1 + monthlyRate, months) - 1) / monthlyRate) *
+      (1 + monthlyRate));
+  var invested = amount * months;
+  // var gainPercent = ((futureValue - invested) / invested) * 100;
+  document.getElementById("resulatinvestamut").value = invested.toFixed();
+  document.getElementById("displayestimateamt").value = futureValue.toFixed();
+ }
--- blocks/sipcalculator/sipcalculator.js
+++ blocks/sipcalculator/sipcalculator.js
@@ -1,100 +1,69 @@
-import {
-    div,
-    label,
-    input,
-    select,
-    option,
-    p,
-    br,
-  } from "../../scripts/dom-helper.js";
-  export default function decorate(block) {
-    const yearsOptions = [1, 3, 5, 10, 15];
-    const counter = [];
-    for (let index = 1; index <= 13; index++) {
-      counter.push(index);
-    }
-    block.append(
-      div(
-        { class: "mainDiv" },
-        div(
-          { class: "amount-div commonclass" },
-          label("Your Monthly SIP Amount"),
-          br(),
-          input({
-            type: "text",
-            value: "5000",
-            class: "amount",
-            onchange: calculateSIP,
-          })
-        ),
-        div(
-          { class: "year-div commonclass" },
-          label("Duration"),
-          br(),
-          select(
-            { class: "years", onchange: calculateSIP },
-            ...yearsOptions.map((year) => {
-              return option({ value: year }, year + " year");
-            })
-          )
-        ),
-        div(
-          { class: "commonclass" },
-          p({ class: "resulatinvestamut" }, "Investment Amt", br(), "60000.00")
-        ),
-        div(
-          { class: "rate-div2 commonclass" },
-          div(
-            { class: "rate-div" },
-            label("Expected Annual Return"),
-            select(
-              { class: "returnRate", onchange: calculateSIP },
-              ...counter.map((element) => {
-                return option({ value: element }, element + " %");
-              })
-            )
-          ),
-          p({ class: "displayestimateamt" }, "60326.00")
-        )
-        //div({ class: "result-div commonclass" }, p({ class: "result" })),
-        //button({ onclick: calculateSIP }, "Calculate")
-      )
-    );
-  }
-  function calculateSIP() {
-    var amount = parseFloat(document.getElementsByClassName("amount")[0].value);
-    var years = parseInt(document.getElementsByClassName("years")[0].value);
-    var returnRate = parseFloat(
-      document.getElementsByClassName("returnRate")[0].value
-    );
-  
-    if (isNaN(amount) || isNaN(years) || isNaN(returnRate)) {
-      document.getElementById("result").innerHTML = "Please enter valid values.";
-      return;
-    }
-  
-    var months = years * 12;
-    var monthlyRate = returnRate / 12 / 100;
-  
-    var futureValue =
-      amount *
-      (((Math.pow(1 + monthlyRate, months) - 1) / monthlyRate) *
-        (1 + monthlyRate));
-    var invested = amount * months;
-    // var gainPercent = ((futureValue - invested) / invested) * 100;
-    document.getElementsByClassName("resulatinvestamut")[0].innerHTML =
-      "Investment Amt" + "<br>" + invested.toFixed();
-    document.getElementsByClassName("displayestimateamt")[0].innerHTML =
-      futureValue.toFixed();
-    //   document.getElementsByClassName("result")[0].innerHTML =
-    //     "Invested Amount: ₹" +
-    //     invested.toFixed(2) +
-    //     "<br>" +
-    //     "Estimated Amount: ₹" +
-    //     futureValue.toFixed(2) +
-    //     "<br>" +
-    //     "Returns: " +
-    //     gainPercent.toFixed(2) +
-    //     "%";
-  }
-  
+// import {
+//     div,
+//     label,
+//     input,
+//     select,
+//     option,
+//     p,
+//     br,
+//   } from "../../scripts/dom-helper.js";
+//   export default function decorate(block) {
+//     const yearsOptions = [1, 3, 5, 10, 15];
+//     const counter = [];
+//     for (let index = 1; index <= 13; index++) {
+//       counter.push(index);
+//     }
+//     block.append(
+//       div(
+//         { class: "mainDiv" },
+//         div(
+//           { class: "amount-div commonclass" },
+//           label("Your Monthly SIP Amount"),
+//           br(),
+//           input({
+//             type: "text",
+//             value: "5000",
+//             class: "amount",
+//             onchange: calculateSIP,
+//           })
+//         ),
+//         div(
+//           { class: "year-div commonclass" },
+//           label("Duration"),
+//           br(),
+//           select(
+//             { class: "years", onchange: calculateSIP },
+//             ...yearsOptions.map((year) => {
+//               return option({ value: year }, year + " year");
+//             })
+//           )
+//         ),
+//         div(
+//           { class: "commonclass" },
+//           p({ class: "resulatinvestamut" }, "Investment Amt", br(), "60000.00")
+//         ),
+//         div(
+//           { class: "rate-div2 commonclass" },
+//           div(
+//             { class: "rate-div" },
+//             label("Expected Annual Return"),
+//             select(
+//               { class: "returnRate", onchange: calculateSIP },
+//               ...counter.map((element) => {
+//                 return option({ value: element }, element + " %");
+//               })
+//             )
+//           ),
+//           p({ class: "displayestimateamt" }, "60326.00")
+//         )
+//         //div({ class: "result-div commonclass" }, p({ class: "result" })),
+//         //button({ onclick: calculateSIP }, "Calculate")
+//       )
+//     );
+//   }
+import fgbvcbnhghn from "../form/form.js";
+export default function decorate(block) {
+  const blockcontent = block.textContent.trim();
+  console.log(blockcontent);
+  fgbvcbnhghn(block, blockcontent);
+}

b12fc086ab9186c4e67f2a919b8f89ed8105ed9f
akanksha-11-03 <Akanksha.Kasar@teknopoint.local>
2025-06-13 17:11:08 +0530

    new swiper add

---
+++ blocks/carousel-block/carousel-block.js
@@ -0,0 +1,10 @@
+import SwiperBlock from "../swiper/swiper.js"
+export default function decorate(block) {
+  Array.from(block.children).forEach((element, index) => {
+    element.classList.add("inner-" + (index+1))
+    Array.from(element.children).forEach((element, indjx) => {
+      element.classList.add("inner-innerSub-" + (indjx+1))
+    })
+  });
+  SwiperBlock(block)
+}

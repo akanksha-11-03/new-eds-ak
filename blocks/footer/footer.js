f2b76f1a3f7d4f268cc9729db996c706baf6de70
akanksha-11-03 <Akanksha.Kasar@teknopoint.local>
2025-06-13 11:48:35 +0530

    new footer added

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -6,6 +6,7 @@ import { loadFragment } from '../fragment/fragment.js';
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
+  console.log('new footer')
   // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
